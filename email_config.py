1885441e32863e0f10055e82f3bd2f9d5906cdf8
root <daakwuru@gmail.com>
2024-08-10 13:42:59 +0000

    .

--- email_config.py
+++ email_config.py
@@ -6,12 +6,12 @@ conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv('EMAIL'),
     MAIL_PASSWORD = os.getenv('PASS'),
     MAIL_FROM = os.getenv('EMAIL'),
-    MAIL_PORT = 465,
+    MAIL_PORT = 587,
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_FROM_NAME="Study Gateway",
-    MAIL_STARTTLS = False,
+    MAIL_STARTTLS = True,
     USE_CREDENTIALS = True,
-    MAIL_SSL_TLS= True,
+    MAIL_SSL_TLS= False,
     VALIDATE_CERTS = True,
     TEMPLATE_FOLDER='templates'
 )
