e027c9b66c6bb8fc774688ee0d42b5a62f09927c
David Akwuru <daakwuru@gmail.com>
2024-08-10 12:53:40 +0100

    .

--- crud/user_profile_crud.py
+++ crud/user_profile_crud.py
@@ -174,7 +174,7 @@ def get_user_profile(db, current_user):
         MentorStudent.status == 'accepted'
     ).first()
     
-    connection_status = {
+    connection = {
         'connected_mentor': bool(mentor),
         'mentor_id': mentor.mentor_id if mentor else 0,
         'admission_progress': mentor.admission_progress if mentor else 0.0,
